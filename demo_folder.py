cbcfffd890d428306d2915ac8ea8f350c0614974
Trey Woodlief <adwiii7@gmail.com>
2021-07-29 13:56:46 -0400

    Add cmdline arg to only save color mask

--- demo_folder.py
+++ demo_folder.py
@@ -20,6 +20,7 @@ parser.add_argument('--demo-folder', type=str, default='', help='path to the fol
 parser.add_argument('--snapshot', type=str, default='./pretrained_models/cityscapes_best.pth', help='pre-trained checkpoint', required=True)
 parser.add_argument('--arch', type=str, default='network.deepv3.DeepWV3Plus', help='network architecture used for inference')
 parser.add_argument('--save-dir', type=str, default='./save', help='path to save your results')
+parser.add_argument('--color-mask-only', action='store_true', help='only save the color mask')
 args = parser.parse_args()
 assert_and_infer_cfg(args, train_mode=False)
 cudnn.benchmark = False
@@ -70,15 +71,16 @@ for img_id, img_name in enumerate(images):
     colorized = args.dataset_cls.colorize_mask(pred)
     colorized.save(os.path.join(args.save_dir, color_name))
 
-    # save colorized predictions overlapped on original images
-    overlap = cv2.addWeighted(np.array(img), 0.5, np.array(colorized.convert('RGB')), 0.5, 0)
-    cv2.imwrite(os.path.join(args.save_dir, overlap_name), overlap[:, :, ::-1])
+    if not args.color_mask_only:
+        # save colorized predictions overlapped on original images
+        overlap = cv2.addWeighted(np.array(img), 0.5, np.array(colorized.convert('RGB')), 0.5, 0)
+        cv2.imwrite(os.path.join(args.save_dir, overlap_name), overlap[:, :, ::-1])
 
-    # save label-based predictions, e.g. for submission purpose
-    label_out = np.zeros_like(pred)
-    for label_id, train_id in args.dataset_cls.id_to_trainid.items():
-        label_out[np.where(pred == train_id)] = label_id
-        cv2.imwrite(os.path.join(args.save_dir, pred_name), label_out)
+        # save label-based predictions, e.g. for submission purpose
+        label_out = np.zeros_like(pred)
+        for label_id, train_id in args.dataset_cls.id_to_trainid.items():
+            label_out[np.where(pred == train_id)] = label_id
+            cv2.imwrite(os.path.join(args.save_dir, pred_name), label_out)
 end_time = time.time()
 
 print('Results saved.')
